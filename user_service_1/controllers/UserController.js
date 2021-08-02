0f9f7136d56d4613e1b8955df7bd4f1a87e3cea8
DilkiC <dilkicooray97@gmail.com>
2021-08-02 19:41:26 +0530

    user_table

---
+++ user_service_1/controllers/UserController.js
@@ -0,0 +1,21 @@
+const UserSchema=require('../models/UserSchema');
+const registerUser=(req,resp)=>{
+    const user=new UserSchema({
+        fName:req.body.fName,
+        lName:req.body.lName,
+        email:req.body.email,
+        password:req.body.password,
+        avatar:req.body.avatar,
+        userState:req.body.userState,
+        regDate:req.body.regDate,
+        regTime:req.body.regTime,
+
+    });
+    user.save().then(savedResponse=>{
+        resp.status(200).json({message:'Success',state:true})
+    }).catch(savedResponseError=>{
+        resp.status(500).json({message: 'internel server error',state: false,error:savedResponseError})
+    })
+
+};
+module.exports={registerUser}
--- user_service_1/index.js
+++ user_service_1/index.js
@@ -1,6 +1,33 @@
 /* packages we need to plug
 * express
 *mongoose
+* *nodemon-install global- npm i nodemon g
 * cors origin
 * dotenv    (api auth keys.not sharing with git)
 * */
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const port=process.env.USER_PORT;
+
+const app=express();
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/robotikka",
+    {
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        useFindAndModify:false,
+        useCreateIndex:true
+    }
+    ).then(()=>{
+
+        app.listen(port,()=>{
+            console.log('Robotikka user service up and running on ${port}')
+        });
+
+}).catch((error=>{
+    console.log(error);
+}))
